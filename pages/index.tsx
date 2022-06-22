0a296223b85be6dce4bf05b490874778cea04e9f
Alexander Poskorkov <alexeration@gmail.com>
2022-06-23 02:56:27 +0300

    add Htag component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
+import { Htag } from "../components";
+
 export default function Home(): JSX.Element {
   return (
-    <div>
-
-    </div>
+    <>
+      <Htag tag='h1'>Text</Htag>
+      <Htag tag='h2'>Text</Htag>
+      <Htag tag='h3'>Text</Htag>
+    </>
   );
 }
