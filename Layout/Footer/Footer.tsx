e5f0e42e8dcdb01bb3f8c6a1d463a24d756953c0
relitweb <relitweb@gmail.com>
2022-06-25 17:42:59 +0300

    add footer markup

--- Layout/Footer/Footer.tsx
+++ Layout/Footer/Footer.tsx
@@ -1,7 +1,20 @@
 import { FooterProps } from './Footer.props';
-import styles from './Footer.module.css';
+import { P } from '../../components';
 import cn from 'classnames';
+import styles from './Footer.module.css';
 
-export const Footer = ({ ...props }: FooterProps): JSX.Element => {
-	return <div {...props}>footer</div>;
+export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
+	const currentYear = new Date().getFullYear();
+
+	return (
+		<footer className={cn(className, styles.footer)} {...props}>
+			<P>OwlTop © 2020 - {currentYear} Все права защищены</P>
+			<a className={styles.link} href='#'>
+				Пользовательское соглашение
+			</a>
+			<a className={styles.link} href='#'>
+				Политика конфиденциальности
+			</a>
+		</footer>
+	);
 };
